e2f54b1107063ada10d26f604178f095596ebeed
Vipershroom <undertalefan812@gmail.com>
2022-07-31 18:44:30 -0400

    Change player struct to ask for data instead

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-fn input() -> String {
+pub fn input() -> String {
     let mut buf = String::new();
 
     std::io::stdin().read_line(&mut buf).unwrap();
@@ -12,9 +12,17 @@ pub struct Player {
     pub sum_of_deck: i32,
 }
 
+#[allow(clippy::new_without_default)]
 impl Player {
-    pub fn new(name: String, deck: Vec<i32>) -> Player {
-        Player { name: (name), deck: (deck), sum_of_deck: (0) }
+    pub fn new() -> Player {
+        println!("What is your name?");
+        let name = input();
+        let deck: Vec<i32> = vec![];
+        Player {
+            name: (name),
+            deck: (deck),
+            sum_of_deck: (0),
+        }
     }
     pub fn sum(&mut self) {
         let mut sum = 0;
@@ -23,8 +31,9 @@ impl Player {
         }
         self.sum_of_deck = sum
     }
-
     pub fn add_deck(&mut self, num: i32) {
         self.deck.push(num);
     }
 }
+
+fn game_logic(player: Player) {}
